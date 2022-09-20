bb683a2bf5efd075fb747cd3a57b4c8a6b61e27f
lcy <983813520@qq.com>
2022-09-20 22:31:14 +0800

    interview experience

---
+++ designMode/designMode/LeetCode/src/main/offer/offer10_1.java
@@ -0,0 +1,15 @@
+public class offer10_1 {
+
+
+
+    public int fib(int n) {
+
+        return 1;
+    }
+    /*
+                1
+           1        1
+       1        2       1
+    1       3       3       1
+     */
+}
---
+++ designMode/designMode/src/main/java/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+    public static void main(String[] args) {
+        float f =10f;
+//        char c =10.0;
+
+    }
+}
